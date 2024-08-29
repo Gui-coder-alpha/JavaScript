002602daf59f914c71f48227ea610f8d1618f61d
Gui-coder-github <guilhermesamt64@gmail.com>
2024-08-29 11:52:57 -0400

    Atualização

---
+++ Início/ex05/Script.js
@@ -0,0 +1,10 @@
+var mais = window.document.getElementsByTagName('div') [0] //or marca
+mais.style.color = 'blue'
+
+/*var d = window.document.getElementById('dive')
+d.style.background = 'red'*/  //id ou por name, ou por class//
+
+var d = window.document.querySelector('#dive')  //seletor
+d.style.background = 'red'
+
+console.log('Hello, world!')
