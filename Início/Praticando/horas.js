cb029c2670802f59ac2d43aa2ea4fa2c83fe7d87
Gui-coder-github <guilhermesamt64@gmail.com>
2024-08-31 17:00:11 -0400

    Cor

--- Início/Praticando/horas.js
+++ Início/Praticando/horas.js
@@ -7,9 +7,12 @@ function carregar() {
 
     if (hora >= 0 && hora < 12) {
         image1.src = 'imagem manhã.jpg'
+        document.body.style.background = 'rgb(241, 241, 124)'
     } else if (hora >= 12 && hora < 18) {
         image1.src = 'Tarde.jpg'
+        document.body.style.background = 'rgb(230, 180, 105)'
     } else {
         image1.src = 'Noite.jpg'
+        document.body.style.background = 'rgb(74, 63, 134)'
     }
 }
