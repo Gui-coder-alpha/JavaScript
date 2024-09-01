3f321b03e28332c257f929e0eb40c2bad889140d
Gui-coder-github <guilhermesamt64@gmail.com>
2024-09-01 15:21:41 -0400

    Repetições

---
+++ Início/ex11/index.js
@@ -0,0 +1,5 @@
+var contador =  1
+while (contador <= 7) {
+    console.log(`Hello World! Foram contados ${contador}`)
+      contador++//Obrigatório, se não colcocar vai dar ruim. Dá dulicação infinita
+}
---
+++ Início/ex11/index2.js
@@ -0,0 +1,5 @@
+var contador = 1
+do {
+    console.log(`Hello world! Foram contados ${contador}`)
+    contador++
+} while(contador <= 7)
