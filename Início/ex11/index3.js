766829eb368c47a78ebc17418ac5ea69a7932923
Gui-coder-github <guilhermesamt64@gmail.com>
2024-09-02 13:56:34 -0400

    Contador

---
+++ Início/ex11/index3.js
@@ -0,0 +1,7 @@
+console.log('Vamos comecçar!')
+
+var contador = 1
+for (var contador = 1 ; contador <= 10 ; contador+= 1) {
+    console.log(`Hello world! Foram contados ${contador}`)
+}
+console.log('cabou!')
