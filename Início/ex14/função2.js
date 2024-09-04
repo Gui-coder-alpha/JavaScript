c7d426c1e2fa0eeb7f3b9a4c43196b4c9f4e2006
Gui-coder-github <guilhermesamt64@gmail.com>
2024-09-04 15:14:59 -0400

    Funções

---
+++ Início/ex14/função2.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2='coloque um número') {
+    return n1 + n2
+}
+
+console.log(soma(2 , 6))
---
+++ Início/ex14/parima.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        console.log(`É par o número ${n}`)
+    } else {
+        console.log(`O número ${n} não é par`)
+    }
+}
+
+let res = parimpar(19)
