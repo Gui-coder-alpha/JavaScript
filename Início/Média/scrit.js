097c1475a3537216675e997d341276499b3f6bf1
Gui-coder-github <guilhermesamt64@gmail.com>
2024-09-19 17:56:27 -0400

    ATT

--- Início/Média/scrit.js
+++ Início/Média/scrit.js
@@ -1,13 +1,25 @@
 let num = document.getElementById('numero')
 let list = document.getElementById('lista')
-let res = document.getElementById('resultado')
 let valores = []
 
 function colocar() {
-    if (Number(num.value) > 0 && Number(num.value) <= 100) {
-        valores.push(Number(num))
-        list.textContent = `${valores}`
+    if (num.value > 0 && num.value <= 100) {
+        valores.push(Number(num.value))    //Funcionando agora
+
+        let tab = document.createElement('option') //Cria um elemento no select, chamado de option
+        tab.text = `Valor ${Number(num.value)}`     //Aplicando os valores, do array e um pequeno texto
+        list.appendChild(tab)               // Cria um filho, sendo o option, o pai é o select, chamado de list, ou lista
+        console.log(valores)        //Teste para verificar se está armazenando todos os valores necessário
     } else {
-        window.alert('Coloque número válido')
+        window.alert('Error')   //Não cumpriu nenhuma característica, erro.
     }
 }
+
+
+
+
+
+/*let item = document.createElement('option')
+item.text = `${n} x ${c} = ${n*c}`
+item.value =   `tab${c}`
+tab.appendChild(item)*/
---
+++ Início/ex12/js.js
@@ -0,0 +1,8 @@
+function funcao(a, b) {
+    if (a == b) {
+        return
+    }    
+}
+
+var a = 5
+var b = 5
