302f4825a69e122bdbf00e484236a3dc3ef5e090
Gui-coder-github <guilhermesamt64@gmail.com>
2024-09-29 14:07:18 -0400

    Lista to do

--- Início/Listagem/crair.js
+++ Início/Listagem/crair.js
@@ -11,9 +11,17 @@ function criar() {
     document.body.querySelector('.yes').appendChild(clone) 
 
 
-    let botao1 = document.createElement('button')
+    let botao1 = document.createElement('button') //botão de confirmar atividade
     botao1.setAttribute('id', 'confirmado')
     clone.appendChild(botao1)
+    botao1.addEventListener('click', realizado)
+    function realizado() {
+        if (botao1.style.backgroundColor === "rgb(106, 219, 105)") {
+            botao1.style.backgroundColor = "white";
+        } else {
+            botao1.style.backgroundColor = "rgb(106, 219, 105)";
+        }
+    }     //'rgb(105, 219, 105)'
 
 
     let letrap = document.createElement('p') //letras da lista
@@ -21,8 +29,12 @@ function criar() {
     clone.appendChild(letrap) //letras
     letrap.innerHTML = string
 
-    let botao2 = document.createElement('button')
-    botao2.setAttribute('id', 'excluido')
+    let botao2 = document.createElement('button') //Botão de excluir a atividade
+    botao2.setAttribute('id', 'excluido') //atribui id ou classe, juntamente com o nome
     clone.appendChild(botao2)
-
+    botao2.addEventListener('click', excluir)
+    function excluir() {
+        clone.remove()
+    }
 }
+
